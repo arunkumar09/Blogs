81bc66f6fbd112bd9c54cee7687bca8943851157
Arun Nair <davor.suker09@gmail.com>
2022-12-30 08:42:13 -0600

    load data from json.server

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import React, { useState, useEffect } from 'react';
+import './index.css';
 
 function App() {
-  
+  const [theme, setTheme] = useState(
+    localStorage.getItem('theme') || 'light');
+  const toggleTheme = () => {
+    if (theme === 'light') {
+      setTheme('dark');
+    } else {
+      setTheme('light');
+    }
+  };
+  useEffect(() => {
+    localStorage.setItem('theme', theme);
+    document.body.className = theme;
+  }, [theme]);
 
   return (
     //JSX - babel converst this to HTML
-    <div className="App">
+    <div className={`App ${theme}`}>
+      <button onClick={toggleTheme}>Toggle Theme</button>
       <Navbar></Navbar>
       <div className='content'>
         <Home></Home>        
